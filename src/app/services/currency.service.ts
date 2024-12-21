d4f4411918d0d271885b32332670fe1e1aad3cbb
someDori <demetrepanjakidze1@gmail.com>
2024-12-22 02:30:56 +0400

    scary: try to fix issue

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -3,22 +3,23 @@ import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry, map } from 'rxjs/operators';
 import { CurrencyResponse, CurrencyRate } from '../interfaces/currency.interface';
-import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrencyService {
+  private api_key: string = "26c870240d04744f7198ddfb2cacc167";
+  private baseURL: string = "https://apilayer.net/api/live";
 
   constructor(private http: HttpClient) { }
 
   getCurrencyRates(sourceCurrency: string, targetCurrencies: string[]): Observable<CurrencyRate[]> {
     const params = new HttpParams()
-      .set('access_key', environment.apiKeys.currencyExchangeApiKey)
+      .set('access_key', this.api_key)
       .set('currencies', targetCurrencies.join(','))
       .set('source', sourceCurrency)
 
-    return this.http.get<CurrencyResponse>(environment.baseUrls.apilayer, { params })
+    return this.http.get<CurrencyResponse>(this.baseURL, { params })
       .pipe(
         retry(3),
         map(response => this.transformResponse(response, sourceCurrency, targetCurrencies)),
