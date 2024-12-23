f808388dda279e243a917ff25490d9a17e14bcca
someDori <demetrepanjakidze1@gmail.com>
2024-12-23 13:37:07 +0400

    fix: readd environments file

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  apiKeys: {
+    currencyExchangeApiKey: '26c870240d04744f7198ddfb2cacc167'
+  },
+  baseUrls: {
+    apilayer: 'https://apilayer.net/api/live'
+  }
+};
